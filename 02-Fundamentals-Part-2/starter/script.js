65be596c50e92a2e68c1ff7e348ca2dc14806467
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-16 16:37:53 +0530

    introduction to arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -71,3 +71,10 @@
 
 // console.log(fruitProcessor(2, 5));
 
+const friends = ['Micheal', 'Steven', 'Peter'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+console.log(friends.at(0));
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
