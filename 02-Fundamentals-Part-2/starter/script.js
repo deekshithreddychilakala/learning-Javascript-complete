7a0657d7664dc88aae66d699e3a3f4bbc7030094
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-16 20:10:13 +0530

    the while loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -249,3 +249,9 @@
 //         console.log('Lifting weights repetition ', rep, ' 🏋🏻‍♀️');
 //     }
 // }
+
+// let rep = 1;
+// while (rep <= 10) {
+//     console.log('Lifting weights repetition ', rep, ' 🏋🏻‍♀️');
+//     rep++;
+// }
