d623bba81830ef03e545d4ab12e52137dec96b95
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-16 15:14:08 +0530

    functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -20,3 +20,14 @@
 // logger();
 // logger();
 // logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+console.log(fruitProcessor(3, 5));
+console.log(fruitProcessor(4, 5));
+console.log(fruitProcessor(2, 6));
+console.log(fruitProcessor(5, 1));
