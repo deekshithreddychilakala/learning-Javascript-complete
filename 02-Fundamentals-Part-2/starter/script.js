ce0aac8f55cd1338e2c3506871f24192954db357
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-16 20:16:22 +0530

    while loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -255,3 +255,12 @@
 //     console.log('Lifting weights repetition ', rep, ' 🏋🏻‍♀️');
 //     rep++;
 // }
+
+// let dice = Math.trunc(Math.random() * 6) + 1;
+// while (dice !== 6) {
+//     console.log('you rolled a ', dice)
+//     dice = Math.trunc(Math.random() * 6) + 1;
+//     if (dice === 6) {
+//         console.log('loop ends here')
+//     }
+// }
