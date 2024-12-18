8c97533619d96b923260848040aa962a75fa1bde
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-18 23:05:08 +0530

    Project 1: Guess my number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,57 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number';
+// console.log(document.querySelector('.message').textContent);
+
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 40;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+
+document.querySelector('.btn.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        // when there is no input
+        document.querySelector('.message').textContent = '⛔️ No Number!!';
+    } else if (guess === secretNumber) {
+        // Player wins
+        document.querySelector('.message').textContent = '🎉 Correct Number';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.number').style.width = '30rem';
+
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+
+    } else {
+        if (score > 1) {
+            document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too High!!' : '📉 Too Low!!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = '💥 You lost the game!!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+document.querySelector('.btn.again').addEventListener('click', function () {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.message').textContent = 'Start guessing...';
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.guess').value = '';
+});
