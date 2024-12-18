952da4e437f660ff87f3a34970f856d440e23e53
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-18 23:59:51 +0530

    Modal window logic

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const modalEle = document.querySelector('.modal');
+const overlayEle = document.querySelector('.overlay');
+const btnCloseModalEle = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+    modalEle.classList.remove('hidden');
+    overlayEle.classList.remove('hidden');
+}
+const closeModal = function () {
+    modalEle.classList.add('hidden');
+    overlayEle.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModalEle.addEventListener('click', closeModal);
+
+overlayEle.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function ($event) {
+    if ($event.key.toLowerCase() === 'escape' && !modalEle.classList.contains('hidden')) {
+        closeModal();
+    }
+});
