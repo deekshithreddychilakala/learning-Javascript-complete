ca07733d9217bf0c6d855c362f627019cb58feb3
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-27 22:34:27 +0530

    Regular functions vs arrow functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -81,40 +81,91 @@
 // console.log(y === window.y); // false
 // console.log(z === window.z); // false
 
-console.log('--- The this Keyword in Practice ---');
+// console.log('--- The this Keyword in Practice ---');
 
 
-const calcAge = function (birthYear) {
-    console.log(2037 - birthYear);
-    // console.log(this);
-}
+// const calcAge = function (birthYear) {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
 
-calcAge(1991);
+// calcAge(1991);
 
 
-const calcAgeArrow = (birthYear) => {
-    console.log(2037 - birthYear);
-    // console.log(this);
-}
+// const calcAgeArrow = (birthYear) => {
+//     console.log(2037 - birthYear);
+//     // console.log(this);
+// }
 
-calcAgeArrow(1996);
+// calcAgeArrow(1996);
+
+// const jonas = {
+//     year: 1991,
+//     calcAge: function () {
+//         console.log(this);
+//         console.log(2037 - this.year);
+//     }
+// }
+
+// jonas.calcAge();
+
+// const matilda = {
+//     year: 2017
+// }
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f(); // Regular function call, this keyword is undefined in strict mode
+
+var firstName = 'Matilda';
 
 const jonas = {
+    firstName: 'Jonas',
     year: 1991,
     calcAge: function () {
-        console.log(this);
+        // console.log(this);
         console.log(2037 - this.year);
+
+        // Solution 1
+        // const self = this; // self or that
+
+        // const isMillenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        //     // console.log(this.year >= 1981 && this.year <= 1996);
+        // }
+
+        // Solution 2
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        }
+
+        isMillenial();
+    },
+
+    greet: () => {
+        console.log(this);
+        console.log(`Hey ${this.firstName}`);
     }
 }
 
+// jonas.greet();
 jonas.calcAge();
 
-const matilda = {
-    year: 2017
+// Arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
 }
 
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
+addExpr(2, 5);
 
-const f = jonas.calcAge;
-f(); // Regular function call, this keyword is undefined in strict mode
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+}
+
+addArrow(2, 5);
