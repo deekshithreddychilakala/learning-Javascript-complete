b571de3d64a2f71fac5e3cb93067d5bd78bc7607
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-27 15:25:57 +0530

    scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -9,20 +9,24 @@ function calcAge(birthYear) {
 
         if (birthYear >= 1981 && birthYear <= 1996) {
             var millenial = true;
+
+            // Creating new variable with same name as outer scope's variable
             const firstName = 'Steven';
+
+            // Reassigning outer scope's variable
+            output = 'NEW OUTPUT!';
             const str = `Oh, and you're a millenial, ${firstName}`;
             console.log(str);
 
             function add(a, b) {
                 return a + b;
             }
-
-            output = 'NEW OUTPUT!';
         }
 
         // console.log(str); // Error because str is block scoped
         console.log(millenial); // No Error because var is function scoped
         // console.log(add(2, 3)); // Error because add is block scoped
+        console.log(output); // Output is changed because output is reassigned in the block scope of if statement
     }
 
     printAge();
