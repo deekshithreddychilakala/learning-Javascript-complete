15b68b679e0c19044f570cc6d829118869e3b097
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2024-12-28 18:58:11 +0530

    logical assignment operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -210,11 +210,41 @@ const restaurant = {
 
 // The Nullish Coalescing Operator (??)
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
 
-console.log(guests);
+// console.log(guests);
 
-// Nullish: null and undefined (NOT 0 or '')
-const guestsCorrect = restaurant.numGuests ?? 10;
-console.log(guestsCorrect);
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestsCorrect = restaurant.numGuests ?? 10;
+// console.log(guestsCorrect);
+
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi'
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
