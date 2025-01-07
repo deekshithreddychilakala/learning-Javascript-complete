132eea249869ea4c59fcecb6cc2a7027e6e0be5e
Chilakala Venkata Deekshith Reddy <deekshithreddychilakala@gmail.com>
2025-01-07 16:37:08 +0530

    Forkify Project

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -1,13 +1,140 @@
+import * as model from './model';
+import recipeView from './views/recipeView';
+import searchView from './views/searchView';
+import resultsView from './views/resultsView';
+import paginationView from './views/paginationView';
+import bookmarksView from './views/bookmarksView';
+import addRecipeView from './views/addRecipeView';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { MODAL_CLOSE_SECONDS } from './config';
+
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
 
-// https://forkify-api.herokuapp.com/v2
+
+// https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    resultsView.update(model.getSearchResultsPage());
+
+    // 1. Loading Recipe
+    await model.loadRecipe(id);
+    const recipe = model.state.recipe;
+
+    // 2. Rendering Recipe
+    recipeView.render(model.state.recipe);
+
+    bookmarksView.update(model.state.bookmarks);
+  } catch (err) {
+    recipeView.renderError();
+  }
+}
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+
+    resultsView.render(model.getSearchResultsPage());
+
+    // render initial pagination buttons
+    paginationView.render(model.state.search);
+
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (gotoPage) {
+  resultsView.render(model.getSearchResultsPage(gotoPage));
+  paginationView.render(model.state.search);
+}
+
+const controlServings = function (newServings) {
+  // update servings in state
+  model.updateServings(newServings);
+  // update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+}
+
+const controlAddBookMark = function () {
+  // 1. Add/remove bookmark
+  if (!model.state.recipe.bookmarked)
+    model.addBookmark(model.state.recipe);
+  else
+    model.deleteBookmark(model.state.recipe.id);
+
+  // 2.Update recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3.render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+}
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+}
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // spinner
+    addRecipeView.renderSpinner();
+
+    // upload the new recipe
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe)
+
+    recipeView.render(model.state.recipe);
+
+    // display success message
+    addRecipeView.renderMessage();
+
+    // Render Bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // change id in the url
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // close popup
+    setTimeout(() => {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SECONDS * 1000);
+
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
+}
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookMark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  bookmarksView.addHandlerRender(controlBookmarks);
+  addRecipeView._addHandlerUpload(controlAddRecipe);
+}
+
+init();
---
+++ 18-forkify/starter/src/js/views/searchView.js
@@ -0,0 +1,25 @@
+class SearchView {
+    _parentElement = document.querySelector('.search');
+    _data;
+    _errorMessage = 'We could not find that recipe. Please try another one!';
+    _message = '';
+
+    getQuery() {
+        const query = this._parentElement.querySelector('.search__field').value;
+        this._clearInput();
+        return query;
+    }
+
+    addHandlerSearch(handler) {
+        this._parentElement.addEventListener('submit', function (e) {
+            e.preventDefault();
+            handler();
+        });
+    }
+
+    _clearInput() {
+        this._parentElement.querySelector('.search__field').value = '';
+    }
+}
+
+export default new SearchView();
